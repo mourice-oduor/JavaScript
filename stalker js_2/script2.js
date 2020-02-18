9bb86e8172d9d79a7858d83275a1dca62e59edc9
mourice-oduor <otienomaurice364@gmail.com>
2020-02-18 13:11:16 -0500

    initial commit

---
+++ stalker js_2/script2.js
@@ -0,0 +1,7 @@
+const firstName = prompt('What is your first name? ')
+const lastName = prompt('What is your last name? ')
+const age = prompt('What is your age? ')
+
+const fullName = firstName + ' ' + lastName
+console.log('Your full name is, ' + fullName )
+console.log('You are ' + age + ' ' +'years old') 
