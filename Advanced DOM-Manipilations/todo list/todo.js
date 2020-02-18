86b8a788fc813a01805c960c6a0561c3aaf8cfa5
mourice-oduor <otienomaurice364@gmail.com>
2020-02-18 14:26:55 -0500

    simple todo list

---
+++ Advanced DOM-Manipilations/todo list/todo.js
@@ -0,0 +1,20 @@
+var lis = document.querySelectorAll('li')
+
+for(var i = 0; i < lis.length; i++){
+    lis[i].addEventListener('mouseover', function(){
+        this.classList.add('selected')
+    })
+    lis[i].addEventListener('mouseout',function(){
+        this.classList.remove('selected')
+    })
+    lis[i].addEventListener('click', function(){
+        this.classList.toggle('done')
+    })
+}
+
+/*firstLI.addEventListener('mouseover', function(){
+    firstLI.style.color = 'green'
+})
+firstLI.addEventListener('mouseout', function(){
+    firstLI.style.color = 'black'
+})*/
